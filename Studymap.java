8f1e55dcfb3c583571ff80e4eb7a322b3c8c0b9e
Han <jongwu90@naver.com>
2021-10-21 01:05:15 +0900

    commit

---
+++ Studymap.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+public class Studymap{
+    public static void main(String args[]){
+        Map<String,Integer> hm = new HashMap<>();
+
+        hm.put("test1", 90);
+        hm.put("test4", 88);
+        hm.put("test3", 100);
+        hm.put("test2", 70);
+
+        System.out.println(hm.toString()); //{test4=88, test2=70, test3=100, test1=90}
+
+        List<String> keySet = new ArrayList<>(hm.keySet());
+
+        Collections.sort(keySet); //키로 오름차순 
+        for(String key:keySet){
+            System.out.println(key+" "+hm.get(key));
+            //test1 90
+            //test2 70
+            //test3 100
+            //test4 88
+        }
+        System.out.println();
+        Collections.sort(keySet,(o1,o2)->o2.compareTo(o1)); //키로 내림차순 
+        for(String key:keySet){
+            System.out.println(key+" "+hm.get(key));
+            //test4 88
+            //test3 100
+            //test2 70
+            //test1 90
+        }
+
+        System.out.println();
+        Collections.sort(keySet,(o1,o2)->hm.get(o1).compareTo(hm.get(o2))); //value로 오름차순 
+        for(String key:keySet){
+            System.out.println(key+" "+hm.get(key));
+            //test2 70
+            //test4 88
+            //test1 90
+            //test3 100
+        }
+
+
+        System.out.println();
+        Collections.sort(keySet,(o1,o2)->hm.get(o2).compareTo(hm.get(o1))); //value로 내림차순 
+        for(String key:keySet){
+            System.out.println(key+" "+hm.get(key));
+            //test3 100
+            //test1 90
+            //test4 88
+            //test2 70
+        }
+
+        hm.remove("test3");
+
+        System.out.println(hm.toString()); //{test4=88, test2=70, test1=90}
+
+        if(hm.containsKey("test4")){ //test4 exist
+            System.out.println("test4 exist");
+        }else{
+            System.out.println("test4 not exist");
+        }
+
+        if(hm.containsKey("test3")){ //test3 not exist
+            System.out.println("test3 exist");
+        }else{
+            System.out.println("test3 not exist");
+        }
+
+    }
+}
