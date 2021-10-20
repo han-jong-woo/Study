c9f335ac0f93f73433738e9e493125bfd8240d98
Han <jongwu90@naver.com>
2021-10-21 00:45:14 +0900

    commit

---
+++ Studylist.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class Studylist{
+    public static void main(String args[]){
+        List<Integer> list = new ArrayList<>();
+
+        List<Integer> list2 = new LinkedList<>();
+
+        list.add(2); //추가
+        list.add(3);
+        list.add(1);
+
+        list2.add(4); //추가
+        list2.add(6);
+        list2.add(5);
+
+        System.out.println(list.toString()); //[3,3,1]
+        System.out.println();
+        System.out.println(list2.toString()); //[4,6,5]
+        System.out.println();
+
+        Collections.sort(list);
+        Collections.sort(list2);
+
+        System.out.println(list.toString()); //[1,2,3]
+        System.out.println();
+        System.out.println(list2.toString()); //[4,5,6]
+        System.out.println();
+
+        Collections.sort(list, (o1,o2)->o2.compareTo(o1));
+        Collections.sort(list2, (o1,o2)->o2.compareTo(o1));
+
+        System.out.println(list.toString()); //[3,2,1]
+        System.out.println();
+        System.out.println(list2.toString()); //[6,5,4]
+        System.out.println();
+
+        list.remove(1);
+        list2.remove(1);
+
+        System.out.println(list.toString()); //[3,1]
+        System.out.println();
+        System.out.println(list2.toString()); //[6,4]
+        System.out.println();
+
+    }
+}
