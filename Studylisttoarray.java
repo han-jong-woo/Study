cef54204d6e10b5ac1cdf27868bb9728f991309f
Han <jongwu90@naver.com>
2021-10-21 23:12:22 +0900

    commit

--- Studylisttoarray.java
+++ Studylisttoarray.java
@@ -15,5 +15,23 @@ public class Studylisttoarray {
 		
 		System.out.println(Arrays.toString(arr2)); //[1, 2, 3, 4]
 
+        
+        // 1. mapToInt에 메서드 참조를 이용하는 방법
+        int[] b = Arrays.stream(arr2).mapToInt(Integer::intValue).toArray();
+        
+        // 2. mapToInt에 람다식을 이용하는 방법 
+        int[] c = Arrays.stream(arr2).mapToInt(i -> i).toArray();
+
+		int[] array = {1, 2, 3, 4};
+        Integer[] b2 = Arrays.stream(array).boxed().toArray(Integer[]::new);
+
+		System.out.println(b.getClass().getSimpleName()); // int[]
+		System.out.println(Arrays.toString(b)); //[1, 2, 3, 4]
+
+		System.out.println(c.getClass().getSimpleName()); //int[]
+		System.out.println(Arrays.toString(c)); //[1, 2, 3, 4]
+
+		System.out.println(b2.getClass().getSimpleName()); //Integer[]
+		System.out.println(Arrays.toString(b2)); //[1, 2, 3, 4]
     }
 }
